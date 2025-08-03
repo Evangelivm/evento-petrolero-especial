e529a8566376b3c6627b9bdab5fa85c886bd4bb6
Devy Salomon <devysalomon@gmail.com>
2025-08-02 22:41:17 -0500

    cambios en config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // In dev, do not change!!
+  //   output: "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
